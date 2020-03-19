5d0123c0698b5443fea75ae8595d7d0e10410d77
guptaprakhariitr <prakharpguptag@gmail.com>
2020-03-19 13:27:32 +0530

    edits in previous

--- app/src/main/java/com/example/clone_insta/ui/LoginActivity.java
+++ app/src/main/java/com/example/clone_insta/ui/LoginActivity.java
@@ -1,5 +1,17 @@
 package com.example.clone_insta.ui;
 
-public class LoginActivity {
+import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.clone_insta.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+    }
 }
