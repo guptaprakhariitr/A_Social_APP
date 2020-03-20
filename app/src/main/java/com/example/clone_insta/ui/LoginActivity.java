35f8ccf218c5d74bb10adc434652c5dfc2d86b74
guptaprakhariitr <prakharpguptag@gmail.com>
2020-03-20 14:03:03 +0530

    sign up and login

--- app/src/main/java/com/example/clone_insta/ui/LoginActivity.java
+++ app/src/main/java/com/example/clone_insta/ui/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.example.clone_insta.ui;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.clone_insta.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,5 +19,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
     }
-
+public void signUp(View view)
+{
+    Intent intent = new Intent(LoginActivity.this,
+            signUp.class);
+    startActivity(intent);
+}
 }
--- app/src/main/java/com/example/clone_insta/ui/signUp.java
+++ app/src/main/java/com/example/clone_insta/ui/signUp.java
@@ -1,19 +1,26 @@
 package com.example.clone_insta.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.clone_insta.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signUp extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private EditText email,pass;
+    private EditText email,pass,first,last,country;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +30,25 @@ public class signUp extends AppCompatActivity {
     {
       String email_s=email.getText().toString();
       String pass_s=pass.getText().toString();
+        mAuth.signInWithEmailAndPassword(email_s, pass_s)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("sign in done", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
 
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("fail", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(signUp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+                    }
+                });
     }
 }
