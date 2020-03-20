0e3ceb4fee534d3f92274dfac2b091259c38ab7d
guptaprakhariitr <prakharpguptag@gmail.com>
2020-03-20 13:37:56 +0530

    new user layout created

--- app/src/main/java/com/example/clone_insta/ui/signUp.java
+++ app/src/main/java/com/example/clone_insta/ui/signUp.java
@@ -1,6 +1,9 @@
 package com.example.clone_insta.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +11,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.clone_insta.R;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
+public class signUp extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private EditText email,pass;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.fragment_new_user);
     }
+    public void createAccount(View view)
+    {
+      String email_s=email.getText().toString();
+      String pass_s=pass.getText().toString();
 
+    }
 }
